6b71a1faad2288af0557a26b741b32899ca393d5
Emil Kaspersen <emil@Emil-sin-MacBook-Pro.local>
2025-11-30 21:28:06 +0100

    Adds test script with validator instances and gitignore file.

---
+++ test_validator.py
@@ -0,0 +1,6 @@
+from user_input_validator import UserInputValidator
+
+validator_A = UserInputValidator()
+validator_B = UserInputValidator()
+
+print("Validators created. Ready for the next step.")
